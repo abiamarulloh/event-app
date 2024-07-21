524667da651488d832b53641f5c3ceee53e87c08
abi amarulloh <abiamarulloh06@gmail.com>
2024-07-21 15:30:54 +0700

    fix: mixed http / htpps

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=poppins:300,400,500,600" rel="stylesheet" />
@@ -14,7 +12,31 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        @if (config('app.env') === 'production')
+            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        @endif
+
+        <!-- Primary Meta Tags -->
+        <title>Eventku: Platform Terbaik untuk Mengelola dan Menemukan Event Menarik</title>
+        <meta name="title" content="Eventku: Platform Terbaik untuk Mengelola dan Menemukan Event Menarik" />
+        <meta name="description" content="Temukan dan kelola berbagai event menarik dengan mudah di Eventku. Dari acara lokal hingga festival besar, jadikan setiap momen spesial dengan solusi event management kami yang intuitif. Kunjungi eventku.site untuk info lebih lanjut!" />
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://eventku.site" />
+        <meta property="og:title" content="Eventku: Platform Terbaik untuk Mengelola dan Menemukan Event Menarik" />
+        <meta property="og:description" content="Temukan dan kelola berbagai event menarik dengan mudah di Eventku. Dari acara lokal hingga festival besar, jadikan setiap momen spesial dengan solusi event management kami yang intuitif. Kunjungi eventku.site untuk info lebih lanjut!" />
+        <meta property="og:image" content="https://eventku.site/public/storage/logo-color.png" />
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://eventku.site" />
+        <meta property="twitter:title" content="Eventku: Platform Terbaik untuk Mengelola dan Menemukan Event Menarik" />
+        <meta property="twitter:description" content="Temukan dan kelola berbagai event menarik dengan mudah di Eventku. Dari acara lokal hingga festival besar, jadikan setiap momen spesial dengan solusi event management kami yang intuitif. Kunjungi eventku.site untuk info lebih lanjut!" />
+        <meta property="twitter:image" content="https://eventku.site/public/storage/logo-color.png" />
+
+
+        <link rel="shortcut icon" href="https://eventku.site/public/storage/logo-color.png" type="image/x-icon">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
